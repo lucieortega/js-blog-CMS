0eb0d68708f57bcb60d09928680df2cd6d928e94
lucieortega <102424066+lucieortega@users.noreply.github.com>
2022-04-10 12:28:04 -0400

    added a new page

---
+++  part9.php
@@ -0,0 +1,13 @@
+<?php
+    ini_set('display_errors', 1);     //SHOW ERRORS FOR DEBUGGING
+
+$firstName =    "Lucie" ;                // String type variable
+$lastName = "Ortega" ;                   // String type variable
+$age = 28;                               // Integrer type variable
+$married = true;                         // Boolean type variable
+$childrenNames = array("Alice", "Bob");  // Array type variable
+
+$fullName = $firstName . " " . $lastName        // Concatenation operator
+$dogYears = $age/7;                             // Arithmetic operator
+$isAdult = $isAdult and $married;               // Integrer type variable
+?>
