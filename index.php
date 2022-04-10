3bfd4853fc9f3ee9e92ec8051a455509150126df
lucieortega <102424066+lucieortega@users.noreply.github.com>
2022-04-09 23:43:06 -0400

    changed html file in php

--- index.php
+++ index.php
@@ -9,8 +9,11 @@
 		<h1>Welcome</h1>
 		<h2>Bienvenue - Bienvenidos</h2>
 		<div class="container turquoise topBotomBordersOut">
-			<a href="post-submission.php">post-submission</a>
-			<a href="post.php">post</a>
+			<a href="collection.html">Collection Spring 2021</a>
+			<a href="about.html">About Us</a>
+			<a href="ourproducts.html">Products</a>
+			<a href="prints.html">Prints</a>
+			<a href="contact.html">Contact</a>
 		</div>
 		<nav>
 		<a href="https://www.instagram.com/luciernaga.maison/" target="_blank" rel="noopener noreferrer">
