45e483f14e5c46f771df59d5149b9c17cf25d68b
Jes <38058308+jesababon@users.noreply.github.com>
2022-10-01 23:07:44 -0400

    Update header.php
    Sending suggested changes so you're all set up for module 4.

--- header.php
+++ header.php
@@ -1,4 +1,8 @@
-<!doctype HTML>
 <h1>Welcome</h1>
-		<h2>Bienvenue - Bienvenidos</h2>
-</html>
+	<h2>Bienvenue - Bienvenidos</h2>
+		<nav>
+			<a href="index.php">Index</a>
+			<a href="https://www.instagram.com/luciernaga.maison/" target="_blank" rel="noopener noreferrer">
+				INSTAGRAM </a>
+			<a href="post-submission.php">Post-submission</a>
+		</nav>
