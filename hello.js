1f05505b01457d413a3dc09c1e3376464858309c
lucieortega <102424066+lucieortega@users.noreply.github.com>
2022-10-09 18:25:52 -0400

    adding php variables to it = function

--- hello.js
+++ hello.js
@@ -1,2 +1,12 @@
 alert("Hello World");
 <script src="hello.js"></script>
+var firstName = "Jane";                     // String type variable
+var lastName = "Doe";                       // String type variable
+var age = 35;                               // Number type variable
+var married = true;                         // Boolean type variable
+var childrenNames = ["Alice", "Bob"];       // Array type variable
+var husband = { firstName: "John", lastName: "Doe", age: 35 }    
+                                            // Object type variable
+           function getFullName(firstName, lastName) {     // Function name
+           return  firstName + " " + lastName;         // Returned value
+            }
